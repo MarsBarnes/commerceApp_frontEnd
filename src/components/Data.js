aa04d750ad3d65f17cd6af264b3090faa103a5e4
Abigail125 <abigailbarnes27@gmail.com>
2023-09-05 11:30:43 -0400

    Shop feed and product tiles added

---
+++ src/components/Data.js
@@ -0,0 +1,105 @@
+export const Data = [  {
+    "id": "c51678fe-6ea2-483f-9465-e42fbb1d7f43",
+    "product_name": "Magical Fairy Sticker",
+    "product_description": "Believe in magic with this enchanting fairy sticker",
+    "color": "Purple",
+    "quantity_available": 25,
+    "price": "$2.79"
+  },
+  {
+    "id": "b071db9d-0cd2-43cf-ae9d-b58068444a1d",
+    "product_name": "Fruit Friends Sticker Set",
+    "product_description": "Adorable fruit characters to brighten up your day",
+    "color": "Yellow",
+    "quantity_available": 30,
+    "price": "$3.99"
+  },
+  {
+    "id": "ad3c076a-aaa9-4de4-9f61-d444819fd183",
+    "product_name": "Galactic Adventure Sticker",
+    "product_description": "Embark on a galactic adventure with this sticker",
+    "color": "Purple",
+    "quantity_available": 25,
+    "price": "$2.79"
+  },
+  {
+    "id": "a50bf830-7445-4dc6-9a82-ed482362a3d8",
+    "product_name": "Nature Lover Sticker",
+    "product_description": "Celebrate nature with this scenic sticker",
+    "color": "Green",
+    "quantity_available": 35,
+    "price": "$2.49"
+  },
+  {
+    "id": "986cea75-2558-47d2-81d8-a0d9f35f124c",
+    "product_name": "Space Cats Sticker Pack",
+    "product_description": "Combining space and cats for a unique sticker experience",
+    "color": "Multi",
+    "quantity_available": 15,
+    "price": "$5.99"
+  },
+  {
+    "id": "9028a9e5-5088-4e90-96a0-5a2b511ba437",
+    "product_name": "Emoji Expressions Sticker Pack",
+    "product_description": "Express yourself with a variety of emoji stickers",
+    "color": "Multi",
+    "quantity_available": 15,
+    "price": "$5.99"
+  },
+  {
+    "id": "89c7a3ff-ac29-432b-887f-35fd70e7c732",
+    "product_name": "Happy Bee Sticker",
+    "product_description": "Spread positivity with this cute bee sticker",
+    "color": "Yellow",
+    "quantity_available": 40,
+    "price": "$1.99"
+  },
+  {
+    "id": "8737e1a1-a4b2-48c6-a83c-76a1db8d25a1",
+    "product_name": "Cool Cat Sticker",
+    "product_description": "Adorable cat sticker with sunglasses",
+    "color": "Blue",
+    "quantity_available": 50,
+    "price": "$2.99"
+  },
+  {
+    "id": "8092156b-eb1c-4f71-b954-a7394e6d51ff",
+    "product_name": "Space Explorer Sticker",
+    "product_description": "Explore the universe with this space-themed sticker",
+    "color": "Black",
+    "quantity_available": 30,
+    "price": "$3.49"
+  },
+  {
+    "id": "7e0be52f-491d-41e6-9369-feff75ffe6c8",
+    "product_name": "dinosaur sticker",
+    "product_description": "sticker with a dinosaur on it",
+    "color": "Red",
+    "quantity_available": 0,
+    "price": "$2.00"
+  },
+  {
+    "id": "643c9380-6b22-4cb0-91dd-c757c33d2d87",
+    "product_name": "Cute Critters Sticker Pack",
+    "product_description": "Collect cute critters from around the world",
+    "color": "Multi",
+    "quantity_available": 40,
+    "price": "$1.99"
+  },
+  {
+    "id": "51d4fc02-78b8-4642-acb9-ca1df978f4e5",
+    "product_name": "Whimsical Woodland Sticker",
+    "product_description": "Explore the whimsical wonders of the woodland",
+    "color": "Green",
+    "quantity_available": 20,
+    "price": "$4.49"
+  },
+  {
+    "id": "4e42a81c-0f46-4e80-928f-fd51101954dc",
+    "product_name": "Underwater World Sticker",
+    "product_description": "Dive into the beauty of the underwater world",
+    "color": "Blue",
+    "quantity_available": 20,
+    "price": "$4.49"
+  }
+]
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
+import {Data} from "./Data";
+import { Product } from "./Product";
+
+
+
 
 const Shop = () => {
+  const [productData, setProductData] = useState(Data);
   return (
     <div className="">
-      <h1>Shop</h1>
+      {productData?.map((i, index) => (
+        <Product i={i} index={index} />
+      ))}
     </div>
   );
 };
 
 export default Shop;
+
+
+  
