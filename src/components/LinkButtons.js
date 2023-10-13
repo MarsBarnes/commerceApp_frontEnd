e61ddff4327b9dfe7028c50f0b70e1a35fad9e1c
Mars Barnes <109761287+MarsBarnes@users.noreply.github.com>
2023-10-13 12:58:18 -0400

    link buttons to repo and portfolio different responsive in size

--- src/components/LinkButtons.js
+++ src/components/LinkButtons.js
@@ -1,6 +1,7 @@
 function LinkButtons () {
-    return (
-      <div className="flex linkButtonDiv">
+  return (
+    <>
+      <div className="flex longLinks linkButtonDiv">
         <a
           href="https://marsbarnes.github.io/Portfolio/"
           className="btn btn-primary linkButtons"
@@ -20,7 +21,28 @@ function LinkButtons () {
           To Back End GitHub Repository
         </a>
       </div>
-    );
+      <div className="flex shortLinks linkButtonDiv">
+        <a
+          href="https://marsbarnes.github.io/Portfolio/"
+          className="btn btn-primary linkButtons"
+        >
+          Portfolio
+        </a>
+        <a
+          href="https://github.com/MarsBarnes/commerceApp_frontEnd"
+          className="btn btn-primary linkButtons"
+        >
+          Front Repo
+        </a>
+        <a
+          href="https://github.com/MarsBarnes/commerceApp_backEnd"
+          className="btn btn-primary linkButtons"
+        >
+          Back Repo
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default LinkButtons;
